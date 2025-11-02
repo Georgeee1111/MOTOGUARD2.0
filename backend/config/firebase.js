fd5b9099e7868f849ac4858ee5ed8471812213b3
Georgee O. Minguito <160137694+Georgeee1111@users.noreply.github.com>
2025-11-02 14:08:13 +0800

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,11 +2,25 @@ const admin = require("firebase-admin");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const serviceAccount = require("../serviceAccountKey.json");
+let firebaseParams = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN || "googleapis.com",
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://motoguard-84bdb-default-rtdb.firebaseio.com", 
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseParams),
+    databaseURL: process.env.FIREBASE_DB_URL,
+  });
+}
 
 module.exports = admin;
